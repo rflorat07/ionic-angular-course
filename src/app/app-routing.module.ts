5612f7dadfa4d7194e18a97f953eb672a27d87a6
Roger Florat <rflorat@cidiverte.it>
2019-03-20 16:44:13 +0100

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './auth/auth.guard';
+
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: 'places', pathMatch: 'full' },
+  { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
+  {
+    path: 'places',
+    loadChildren: './places/places.module#PlacesPageModule',
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'bookings',
+    loadChildren: './bookings/bookings.module#BookingsPageModule',
+    canLoad: [AuthGuard]
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
